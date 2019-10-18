7449ff081a4d64315330b745e9e9eeb7998d2580
Piotr Mardziel <piotrm@gmail.com>
2019-10-18 13:15:50 -0700

    typo

--- serve.py
+++ serve.py
@@ -1,22 +1,27 @@
+# for python3
+
 from http import server
 from urllib.parse import unquote
 import base64
 from time import gmtime, strftime
 
+
 class Handler(server.SimpleHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers.get('content-length'))
-        print("post", length)
         data = unquote(str(self.rfile.read(length)))
         png = data.split('base64,')[1]
 
         filename = strftime("%Y-%m-%d_%H-%M-%S.png", gmtime())
 
         with open(filename, "wb") as f:
-            f.write(base64.decodestring(png.encode()))
+            f.write(base64.decodebytes(png.encode()))
+
+        print(f"saved {filename}")
 
         self.send_response(server.HTTPStatus.OK)
         self.end_headers()
+
 
 def run(server_class=server.HTTPServer,
         handler_class=Handler):
@@ -24,5 +29,6 @@ def run(server_class=server.HTTPServer,
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
+
 
 run()
